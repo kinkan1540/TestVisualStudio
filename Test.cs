c8e574382697576a9de758787de516933e1ca50b
kinkan1540 <daigorou4312@gmail.com>
2019-05-09 11:49:59 +0900

    Test

---
+++ Test.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class Test
+{
+    private int x, y;
+
+    private void Test()
+    {
+        x = 1;
+        y = 1;
+        x += y;
+        Console.WriteLine(x);
+    }
+}
